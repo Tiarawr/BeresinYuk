70ca6f5a9400b029337427ef21c0f361f6a8623a
Tiarawr <yumomik@gmail.com>
2025-05-29 02:32:56 +0700

    .

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      Hello, Tailwind CSS + Vite + React!
+    </div>
+  );
+}
