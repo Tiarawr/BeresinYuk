b34869e7db3cdf0788d34af04fea74855da69d55
Affan Ilham Arsyila <l200234024@student.ums.ac.id>
2025-05-31 01:57:55 +0700

    remove Wallpage component and its route from App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Wallpage from "./wallpage";
 import MessageBox from "./messagebox";
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/wallpage" element={<Wallpage />} />
       <Route path="/messagebox" element={<MessageBox/>}/> 
     </Routes>
     </BrowserRouter>
