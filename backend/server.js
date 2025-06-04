a80a27cfcbc5fefdf5f41fcbf15b5cc414a107d7
Naila Yuni Mutiarani <150340542+Tiarawr@users.noreply.github.com>
2025-06-04 15:09:12 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
 const messageRoutes = require("./Routes/message");
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+// DB connect
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
-console.log("🚀 Starting server...");
-require("dotenv").config();
-console.log("✅ .env loaded, MONGO_URI =", process.env.MONGO_URI);
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
